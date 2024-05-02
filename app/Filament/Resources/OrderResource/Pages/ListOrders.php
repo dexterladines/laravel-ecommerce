41a79e623abb0d92c0551807ced939f5aed071f5
Dexter Ladines <ladines.dexter@seedtech.ph>
2024-05-02 11:01:08 +0800

    Add Order Stats and Record Filtering

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
+use App\Models\Order;
 use Filament\Actions;
+use Filament\Forms\Components\Tab;
+use Filament\Resources\Components\Tab as ComponentsTab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -15,5 +19,24 @@ class ListOrders extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+    {
+      return [
+        null => ComponentsTab::make('All'),
+        'new' => ComponentsTab::make()->query(fn ($query) => $query->where('status', 'new')),
+        'processing' => ComponentsTab::make()->query(fn ($query) => $query->where('status', 'processing')),
+        'shipped' => ComponentsTab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+        'delivered' => ComponentsTab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+        'cancelled' => ComponentsTab::make()->query(fn ($query) => $query->where('status', 'cancelled'))
+      ];
     }
 }
